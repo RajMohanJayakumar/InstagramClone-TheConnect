d9274cee7f4aa6e9f110e13e885d9f20e46070c5
unknown <rajmohan2695@gmail.com>
2019-12-05 21:03:39 +0530

    Partial working commit

--- main/java/com/theconnect/App.java
+++ main/java/com/theconnect/App.java
@@ -6,12 +6,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/Service")
-public class Service extends HttpServlet {
+@WebServlet("/App")
+public class App extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		
+		System.out.println("Hello");
+		System.out.println(session.getAttribute("id"));
 	}
-
 }
--- main/webapp/script.js
+++ main/webapp/script.js
@@ -1,14 +1,38 @@
-function onSignIn(googleUser) {
+function onSignIn(googleUser,setSession) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//    profileInfo(profile.getName(),profile.getImageUrl(),profile.getEmail());
+var proId = profile.getId();
+    // console.log('ID: ' + profile.getId()); 
+    console.log('Name: ' + proId);
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
+    console.log('Email: ' + profile.getEmail());
+    setSession(proId);
+    function setSession(id){
+        axios.post('http://localhost:8080/app?id='+id, {})
+        .then(res => {
+        window.location.replace("http://localhost:8080/app.html");
+    })
+      }
+    }
+
+  
+
+//function profileInfo(name,imgUrl,email){
+//	document.getElementById('proName').innerText = name;
+//	let str = ``;
+//	document.getElementById('update').innerText = name;
+//	document.getElementById('proEmail').innerText = email;
+//}
 
   function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
+    window.location.replace("http://localhost:8080/logout.html");
+    var auth2 = gapi.auth2.getAuthInstance().disconnect();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
+  }
+
+  function login(){
+    window.location.replace("http://localhost:8080/index.html");
   }
