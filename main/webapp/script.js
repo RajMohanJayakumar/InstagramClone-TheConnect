6753f3045bfbc47026c5fddff801e9f15c5ca9da
unknown <rajmohan2695@gmail.com>
2019-12-09 23:44:39 +0530

    redirect from frontend using javascript

--- main/webapp/script.js
+++ main/webapp/script.js
@@ -8,22 +8,28 @@ function onSignIn(googleUser,setSession) {
     console.log('Email: ' + profile.getEmail());
     setSession(profile.getId(),profile.getName(),profile.getImageUrl(),profile.getEmail());
     function setSession(id,name,proPic,email,signOut){
-        axios.post('http://localhost:8080/app?id='+id+'&name='+name+'&proPic='+proPic+'&email='+email, {})
-        .then(res => {
-        signOut();
-        function signOut() {
-            var auth2 = gapi.auth2.getAuthInstance().disconnect();
+        // axios.get('http://localhost:8080/app?id='+id+'&name='+name+'&proPic='+proPic+'&email='+email, {})
+        // .then(res => {
+          window.location.replace('http://localhost:8080/app?id='+id+'&name='+name+'&proPic='+proPic+'&email='+email)
+        // signOut();
+        // function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance()
             auth2.signOut().then(function () {
               console.log('User signed out.');
-            });
-          }
+            // });
+          // }
     })
     
       }
     }
 
     function logout(){
-        window.location.replace("http://localhost:8080/login.html");
+        window.location.replace("http://localhost:8080");
+    }
+
+    function updateUserDetails(username,email,imgUrl){
+      document.getElementById('username').innerHTML = "Hi, Raj Mohan";
+      document.getElementById('email').innerHTML = "rajmohan2695@gmail.com";
     }
 
   
