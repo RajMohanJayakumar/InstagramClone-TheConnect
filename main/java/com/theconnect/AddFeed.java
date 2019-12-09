f8eae2b6f3212aff855083b054237c68b10e9d93
unknown <rajmohan2695@gmail.com>
2019-12-09 23:43:36 +0530

    Added POJO and represented class

---
+++ main/java/com/theconnect/AddFeed.java
@@ -0,0 +1,36 @@
+package com.theconnect;
+
+import java.io.Serializable;
+
+public class AddFeed implements Serializable{
+	
+	private String email;
+	private String text;
+    private String photo;
+    private String timeStramp;
+    
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getTimeStramp() {
+		return timeStramp;
+	}
+	public void setTimeStramp(String timeStramp) {
+		this.timeStramp = timeStramp;
+	}
+}
---
+++ main/java/com/theconnect/AddPost.java
@@ -0,0 +1,67 @@
+package com.theconnect;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+/**
+ * Servlet implementation class AddPost
+ */
+@WebServlet("/addpost")
+public class AddPost extends HttpServlet {
+
+	AddFeed addfeed;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String str = jsonToString(request);
+		addfeed = jsonPharse(str);
+		System.out.println(addfeed.getEmail()+addfeed.getPhoto()+addfeed.getText()+addfeed.getTimeStramp());
+		
+	}
+	
+	public String jsonToString(HttpServletRequest request) throws IOException {
+		String line = "";
+		StringBuffer str = new StringBuffer();
+		BufferedReader reader = request.getReader();
+		while((line = reader.readLine()) != null)
+			{
+			str.append(line);
+			}
+		String jsonString = str.toString();
+		return jsonString;
+	}
+	
+	//Pharsing a json standard string format to an Object(Contact)
+	public AddFeed jsonPharse(String str) throws IOException {
+		
+		//Creating reference to store the contact variable
+		AddFeed addfeed = null;
+		
+	      ObjectMapper mapper = new ObjectMapper();
+	      
+	      try
+	      {
+	         addfeed =  mapper.readValue(str.getBytes(),AddFeed.class);
+	      } catch (JsonGenerationException e)
+	      {
+	         e.printStackTrace();
+	      } catch (JsonMappingException e)
+	      {
+	         e.printStackTrace(); 
+	      } catch (IOException e)
+	      {
+	         e.printStackTrace();
+	      }
+	      return addfeed;
+	}
+
+}
